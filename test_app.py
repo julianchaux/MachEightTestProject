63407fb788cd86c2ae99daad8b91d9002b6ccc3f
Julián René Cháux Cedeño <jchaux@misena.edu.co>
2022-10-10 23:48:55 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -8,14 +8,16 @@ class TestApp(unittest.TestCase):
 
     def setUp(self):
         self.total_sum = 10
-        self.numbers = [1,15,14,25,-25,-1,6,4,-15,35]
+        self.numbers = [1,15,14,25,-25,-1,6,4,-15,35,-100,45,89,-10,20]
 
     def test_app(self):
         result = checkSumPair(self.numbers, self.total_sum)
         if len(result):
-            for i in range(len(result)):            
+            for i in range(len(result)):
                 self.assertEqual(result[i][0] + result[i][1], self.total_sum)
+                print(f"The numbers {result[i][0]} and {result[i][1]} add up to {self.total_sum}")                
         else:
+            self.assertEqual(result, [])
             print(f"No pair of numbers in {self.numbers} satisfies the total sum of {self.total_sum}")
 
 if __name__ == '__main__':
